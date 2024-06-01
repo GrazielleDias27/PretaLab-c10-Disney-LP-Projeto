b6bc62b302f53477d8dff4726d9cf093570eb97d
Grazielle Dias <clebiagrazielle@gmail.com>
2024-05-31 21:58:55 -0300

    subindo arquvos

--- src/script.js
+++ src/script.js
@@ -1,27 +1,64 @@
-let numeroAleatorio = 0;
+let numeroAleatorio = Math.floor(Math.random() * 100) + 1;
 let tentativas = 0;
+let palpitesRepitidos = [];
 
 function jogoDeAdivinhacao() {
-    /*
-    Guiado
-    ---
-    1. Não aceita palpite vazio
-    2. Dar dicas a cada palpite (maior ou menor)
-    3. Atualizar a pontuação a cada palpite errado
-    4. Mostrar todos os palpites errados
-    5. Deve-se poder reiniciar o jogo a qualquer momento
-    6. Se a pontuação chegar a zero, deve ser exibido um alerta e reiniciado o jogo
+    const palpiteDigitado = pegarPalpiteDigitado();
+
+
+
+    if (!palpiteDigitado) {
+        alert("Digite um valor válido!")
+        return;
+            
+    }else if (palpiteDigitado <= 0 || palpiteDigitado > 100) {
+        alert("por favor, digite um numero entre 1 e 100.")
+    }if(palpitesRepitidos.includes(palpiteDigitado)) {
+        alert("você já tentou esse palpite");
+        return;
+    }
+
+
+      palpitesRepitidos.push(palpiteDigitado);
+
+    if (palpiteDigitado === numeroAleatorio) {
+        alert("parabéns, você divinhou!")
+        reiniciarJogo();
+        return;
+    } else if (palpiteDigitado > numeroAleatorio) {
+        tentativas++;
+        atualizarFeedback("O número e muito alto.Tente novmente")
+    } else if (palpiteDigitado < numeroAleatorio) {
+        tentativas++;
+        atualizarFeedback("O número e muito baixo.Tente novamente.")
+    }
+
+   
+    const novaPontuacao = 100 - (tentativas * 10);
+    atualizarPontuacao(novaPontuacao);
+
+    const palpitesFalhos = pegarPalpitesFalhos();
+    const novosPalpitesFalhos = palpitesFalhos + " " + palpiteDigitado;
+    atualizarPalpitesFalhos(novosPalpitesFalhos)
+
+    const pegarPontuacaoAtual = pegarPontuacao();
+    if (pontuacaoAtual ==="Você tem 0 pontos") {
+        alert("Deu ruim boy! Você chegou no limbo, acabou pra você!");
+        reiniciarJogo();
+    }
     
-    Individual
-    ---
-    7. Só deve aceitar numeros entre 1 e 100
-    8. Não deve aceitar palpite repetido
-    */
-
-
-    // to-do
 }
-
 function reiniciarJogo() {
-    // to-do
+    const vaiReiniciar = confirm("Deseja jogar novamente");
+    
+    if (vaiReiniciar) {
+        tentativas = 0;
+        palpitesRepitidos = [];
+        numeroAleatorio = Math.floor(Math.random() * 100) + 1;
+        atualizarPalpitesFalhos("");
+        atualizarPontuacao(100);
+        atualizarFeedback("");
+        limparPalpiteDigitado();
+    }
 }
+
